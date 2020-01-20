ebaa1125e6e50a04194adf72c8fa4fdadec7465a
Lostlarry <lostlarrythelizard@gmail.com>
2020-01-20 20:54:01 +0100

    assets

---
+++ Assets/scripts/Line_Ctrl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line_Ctrl : MonoBehaviour
+{
+    Line_Ctrl next;
+    Line_Ctrl prev;
+
+    int id = -1;
+
+    public Line_Ctrl Next { get => next; set => next = value; }
+    public Line_Ctrl Prev { get => prev; set => prev = value; }
+
+    public int Id 
+    {
+        get
+        {
+            return id;
+        }
+        set
+        {
+            if (id == -1)
+            {
+                id = value;
+            }
+        }
+    }
+
+    void Start() { }
+    void Update() { }
+}
---
+++ Assets/scripts/Menu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    Screen_Ctrl Ctrl;
+    Save_Load S_L;
+
+    void Start()
+    {
+        Ctrl = gameObject.GetComponent<Screen_Ctrl>();
+        S_L = gameObject.GetComponent<Save_Load>();
+    }
+    void Update() { }
+
+    public void Create()
+    {
+        Ctrl.Swap_canvas(11);
+    }
+
+    public void doCreate()
+    {
+        Ctrl.Swap_canvas(12);
+    }
+
+    public void cancelCreate()
+    {
+        Ctrl.Swap_canvas(4);
+    }
+
+    public void Load()
+    {
+        S_L.Load();
+        Ctrl.Swap_canvas(13);
+    }
+
+    public void Save()
+    {
+        S_L.Save();
+    }
+
+    public void Play()
+    {
+        Ctrl.Swap_canvas(3);
+    }
+
+    public void unPlay()
+    {
+        Ctrl.Swap_canvas(1);
+    }
+
+    public void Main_Menu()
+    {
+        Ctrl.Swap_canvas(2);
+    }
+}
