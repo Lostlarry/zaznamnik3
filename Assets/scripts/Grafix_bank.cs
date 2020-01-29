3eb426dd249f53631f0bb9d63f34d6bb0a53d29d
Lostlarry <lostlarrythelizard@gmail.com>
2020-01-29 21:23:09 +0100

    imgs

--- Assets/scripts/Grafix_bank.cs
+++ Assets/scripts/Grafix_bank.cs
@@ -9,11 +9,11 @@ public class Grafix_bank : MonoBehaviour
     public Sprite Prefix_becko;
     public Sprite Prefix_neutral;
 
-
-
-
-
-
+    public Sprite Nota_cela;
+    public Sprite Nota_pull;
+    public Sprite Nota_ctvrt;
+    public Sprite Nota_prapor;
+    public Sprite Nota_carka;
 
     // Start is called before the first frame update
     void Start()
